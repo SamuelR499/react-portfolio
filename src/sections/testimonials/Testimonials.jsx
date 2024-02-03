f984f30798194cba86f662778fc653c1824f4e27
SamuelR499 <samuelribeiro2@hotmail.com>
2024-02-02 22:00:01 -0300

    Implementando O slide da seção Testimonials

--- src/sections/testimonials/Testimonials.jsx
+++ src/sections/testimonials/Testimonials.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+
 import './testimonials.css'
+import testimonials from './data';
+import Testimonial from './Testimonial';
 
 function Testimonials() {
   return (
-    <section id="testimonials"><h2>Testimonials</h2></section>
+    <section id="testimonials">
+      <h2>What My Clients Say</h2>
+      <p>These are unbiased testimonials from some of my clients</p>
+      <div className="container">
+          <Swiper
+          spaceBetween={50}
+          slidesPerView={3}
+          onSlideChange={() => console.log('slide change')}
+          onSwiper={(swiper) => console.log(swiper)}
+          className='MySwiper'
+        >
+          {testimonials.map(testimonial => (
+            <SwiperSlide key={testimonial.id}>
+              <Testimonial />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </section>
   )
 }
 
