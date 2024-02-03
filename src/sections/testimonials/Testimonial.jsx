b0e00be51a188b820fd3d266898f1bf3c18666ed
SamuelR499 <samuelribeiro2@hotmail.com>
2024-02-02 22:10:16 -0300

    Criando os elementos para mostrar dados de testimonials

--- src/sections/testimonials/Testimonial.jsx
+++ src/sections/testimonials/Testimonial.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import Card from "../../components/Card";
 
-function Testimonial() {
+
+function Testimonial({testimonial}) {
   return (
-    <div>Testimonial</div>
+    <Card className="Light">
+        <p>
+            {testimonial.quote}
+            <div className="testimonial_client">
+                <div className="testimonial_client-avatar">
+                    <img src={testimonial.avatar} alt="Testimonial Avatar" />
+                </div>
+                <div className="testimonial client-details">
+                    <h6>{testimonial.name}</h6>
+                    <small>{testimonial.profession}</small>
+                </div>
+            </div>
+        
+        </p>
+    </Card>
   )
 }
 
