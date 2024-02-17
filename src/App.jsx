cba52ab673dc50c04ba7347361b8eb570978deb4
SamuelR499 <samuelribeiro2@hotmail.com>
2024-02-17 10:52:30 -0300

    Adicioando o themeState onde a magia acontece no app, trocando classe do main

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,13 @@ import Contact from './sections/contact/Contact';
 import Footer from './sections/footer/Footer';
 import FloatingNav from './sections/floating-nav/FloatingNav';
 import Theme from './theme/Theme';
+import { useThemeContext } from './context/theme-context';
 
 function App() {
+  const {themeState} = useThemeContext();
+
   return (
-    <main>
+    <main className={`${themeState.primary} ${themeState.background}`}>
       <Navbar />
       <Header />
       <About />
